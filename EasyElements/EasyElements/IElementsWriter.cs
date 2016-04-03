db2a0d90e47032c1ec9dc4ebd6eab92d3a65146f
Дмитрий <skillet123q@gmail.com>
2016-04-03 14:01:08 +0300

    implemented to save the file

---
+++ EasyElements/EasyElements/IElementsWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using EasyElements.Configs;
+
+namespace EasyElements
+{
+    public interface IElementsWriter
+    {
+        string Path { get; set; }
+        ElementsData Elements { get; set; }
+        Config Config { get; set; }
+        
+        void Save();
+    }
+}
