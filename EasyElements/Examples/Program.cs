d3c5c194c614caf84bff33fcf3a30732b29ea5c8
Дмитрий <skillet123q@gmail.com>
2016-04-04 22:23:40 +0300

    add example

---
+++ EasyElements/Examples/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyElements;
+using EasyElements.Configs;
+
+namespace EasyElementsExample
+{
+    class Program
+    {
+        private static Stopwatch _stopwatch;
+
+        private static void Main(string[] args)
+        {
+            _stopwatch = Stopwatch.StartNew();
+
+            //Open configuration file
+            var configReader = new ConfigReader("config.xml");
+            var config = configReader.Open();
+
+            ShowInfo("Config opened");
+
+            //Open elements.data
+            var elementsReader = new ElementsReader("elements.data", config);
+            var elementsData = elementsReader.Open();
+
+            ShowInfo("Elements.data opened");
+
+            //Get data
+            foreach (DataRow row in elementsData.Data.Tables["012 - MEDICINE SUB TYPE"].Rows)
+                Console.WriteLine($"ID: {row["ID"]} \t Name: {row["Name"]}");
+
+            ShowInfo("Get data completed");
+
+            //Get type row and list
+            foreach (var confList in elementsData.ConfigForThisElements.Lists)
+            {
+                var elementsList = elementsData.Data.Tables[confList.Name];
+                Console.WriteLine($"Count {confList.Caption}: {elementsList.Rows.Count}");
+            }
+
+            //Save elements.data
+            var elementsWriter = new ElementsWriter(elementsReader);
+            elementsWriter.Save();
+
+            ShowInfo("Elements.data saved");
+
+
+
+
+            _stopwatch.Stop();
+            Console.ReadKey();
+        }
+
+        private static void ShowInfo(string text)
+        {
+            _stopwatch.Stop();
+
+            Console.WriteLine($"{text}: {_stopwatch.Elapsed}");
+
+            _stopwatch = Stopwatch.StartNew();
+        }
+    }
+}
---
+++ EasyElements/Examples/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("EasyElementsExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EasyElementsExample")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b1733912-92f0-4c9d-bf3e-06d4841094ad")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
